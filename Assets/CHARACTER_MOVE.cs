edfddd8fff39cfe314df31986138ea5f0540e0e4
Azrael <j.shepard.ramsay@gmail.com>
2015-12-11 17:18:54 -0500

    textured level; added walls

---
+++ Assets/CHARACTER_MOVE.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CHARACTER_MOVE : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
